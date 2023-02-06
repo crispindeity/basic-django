f28f47fb46ea21d9d6f528ac363b2920ea55d424
crispindeity <shoy1415@gmail.com>
2023-02-06 22:38:51 +0900

    :sparkles: feat index view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from app.models import Post
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.all().order_by("-pk")
+    return render(
+        request,
+        "app/index.html",
+        {
+            "posts": posts,
+        },
+    )
